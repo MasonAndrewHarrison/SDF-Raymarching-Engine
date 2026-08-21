5c0b8af17214d09f1cf685fdd0754f131ac41163
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2026-08-20 21:42:28 -0400

    update code logic

--- camera.h
+++ camera.h
@@ -17,7 +17,11 @@ extern "C" {
 #endif
 void updateRayOrigin();
 
-
+inline void updateRayOrigin(){
+    state.rayOriginZ = state.cameraDistance * sin(state.cameraTheda + M_PI/2) * cos(state.cameraPhi) + state.focusZ;
+    state.rayOriginY = state.cameraDistance * cos(state.cameraTheda + M_PI/2) + state.focusY;
+    state.rayOriginX = state.cameraDistance * sin(state.cameraTheda + M_PI/2) * sin(state.cameraPhi) + state.focusX;
+}
 
 #ifdef __cplusplus
 }
